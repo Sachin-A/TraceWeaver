01b81fd720344ef47fe49888049a08733fe9ee47
Sachin Ashok <sachinashok.g@gmail.com>
2023-02-20 13:32:44 -0600

    Create server record and standardize format

--- ports/python/alibaba-analysis/real-parser.py
+++ ports/python/alibaba-analysis/real-parser.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import json
+import shutil
 import random
 import string
 import tarfile
@@ -9,8 +11,8 @@ from distutils.version import LooseVersion
 
 search_strings = ["(?)", "NAN", "nan", ""]
 
-def compress(output_filename, source_dir):
-    with tarfile.open(output_filename, "w:gz") as tar:
+def compress(output_filename, source_dir, format = "w:gz"):
+    with tarfile.open(output_filename, format) as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 def uncompress(output_path, source_file):
@@ -94,8 +96,7 @@ def checkCopyRelaxed(span1, span2, col):
     sublist2 = [span2[x] for x in indices]
     if sublist1 == sublist2:
         if ((int(span1[8]) > 0 and int(span2[8]) < 0) or
-            (int(span1[8]) < 0 and int(span2[8]) > 0) or
-            (int(span1[8]) == 0 and int(span2[8]) == 0)):
+            (int(span1[8]) < 0 and int(span2[8]) > 0)):
             return True
 
     return False
@@ -167,7 +168,7 @@ def checkNeighbours(trace, row, col, rpc_id_count):
             span[3] == rpc_id and
             not missingCol(span[col]) and
             rpc_id_count.get(span[3], 0) == 2 and
-            checkCopyStrict(trace[row], span, col)):
+            checkCopyRelaxed(trace[row], span, col)):
                 trace[row][col] = str(span[col])
                 return True
 
@@ -279,37 +280,86 @@ def checkOrphans(trace):
 
 def buildCallGraph(trace):
     trace_cg = {}
+    span_id_to_index = {}
     trace_cg['root'] = trace[0][3]
     len_root = len(trace[0][3].split("."))
 
     for i, span in enumerate(trace):
+        span_id_to_index[span[3]] = i
         if i != 0 and len(span[3].split(".")) == len_root:
-            print(trace[0])
-            print(span)
-            print("A")
-            input()
-            return {}, False
+            return {}, {}, False
 
         if span[3] not in trace_cg:
             trace_cg[span[3]] = []
         else:
-            print("B")
-            return {}, False
+            return {}, {}, False
 
         if i != 0:
             parent_rpc_id = ".".join(span[3].split(".")[:-1])
             if parent_rpc_id in trace_cg:
                 trace_cg[parent_rpc_id].append(span[3])
             else:
-                print("C")
-                return {}, False
+                return {}, {}, False
 
-    return trace_cg, True
+    return trace_cg, span_id_to_index, True
+
+def convertToJaegerFormat(trace, trace_cg, span_id_to_index, dataset_id):
+    trace_dict = {}
+    trace_dict['data'] = []
+
+    trace_data = {}
+    trace_data['traceID'] = trace[0][1]
+    trace_data['spans'] = []
+
+    for j, span in enumerate(trace):
+        if span[3] not in span_id_to_index:
+            print("Skipped a span")
+            continue
+
+        client_record = {}
+        client_record['traceID'] = span[1]
+        client_record['spanID'] = span[3]
+        client_record['startTime'] = int(span[2]) * 1000
+        client_record['duration'] = abs(int(span[8]) * 1000)
+
+        client_record['tags'] = []
+        tags_data = {}
+        tags_data['key'] = "span.kind"
+        tags_data['value'] = "client"
+        client_record['tags'].append(tags_data)
+
+        client_record['references'] = []
+        references_data = {}
+        if span[3] == trace_cg['root']:
+            pass
+        else:
+            references_data['refType'] = "CHILD_OF"
+            parent_rpc_id = ".".join(span[3].split(".")[:-1])
+            references_data['traceID'] = span[1]
+            references_data['spanID'] = parent_rpc_id
+
+        client_record['references'].append(references_data)
+        client_record['processID'] = span[4]
+        trace_data['spans'].append(client_record)
+
+        if span[3] == trace_cg['root']:
+            pass
+        else:
+            server_record = dict(client_record)
+            server_record['tags'][0]['value'] = "server"
+            server_record['processID'] = span[6]
+            trace_data['spans'].append(server_record)
+
+    trace_dict['data'].append(trace_data)
+
+    Path(path + 'traces' + str(dataset_id) + '/').mkdir(parents = True, exist_ok = True)
+    with open(path + 'traces' + str(dataset_id) + '/' + trace_data['traceID'] + '.json', 'w', encoding = 'utf-8') as f:
+        json.dump(trace_dict, f, ensure_ascii = False, indent = 4)
 
 path = "/scratch/sachina3/projects/clusterdata/cluster-trace-microservices-v2021/data/MSCallGraph/"
 
-# num_shards = 144
-num_shards = 0
+num_shards = 144
+# num_shards = 0
 overall_trace_ids = [[] for i in range(num_shards + 1)]
 original_dataset = {}
 overall_missing = 0
@@ -322,13 +372,13 @@ overall_root_missing = 0
 overall_multi_root = 0
 distinct_roots = set()
 overall_not_valid = 0
+overall_valid = 0
+trace_count = 0
 
-for dataset_id in range(0, num_shards + 1):
+for dataset_id in range(1, num_shards + 1):
 
     shard_directory = path + "shard" + str(dataset_id) + "/"
     trace_paths = getAllTracesInDir(shard_directory)
-
-    trace_count = 0
 
     for trace_path in trace_paths:
 
@@ -343,6 +393,7 @@ for dataset_id in range(0, num_shards + 1):
                   "\nRoot missing: ", overall_root_missing, (overall_root_missing * 100) / trace_count,
                   "\nMulti-root (d): ", overall_multi_root, (overall_multi_root * 100) / trace_count,
                   "\nNot valid: ", overall_not_valid, (overall_not_valid * 100) / trace_count,
+                  "\nValid: ", overall_valid, (overall_valid * 100) / trace_count,
                   "\nDistinct roots: ", len(distinct_roots),
                   "\nNo errors (a, b, c, d): ", no_errors, (no_errors * 100) / trace_count)
 
@@ -386,9 +437,12 @@ for dataset_id in range(0, num_shards + 1):
 
         if a == 0 and (b1 + b2) == 0 and c == 0 and d == 0:
             no_errors += 1
-
-            trace_cg, valid = buildCallGraph(trace)
+            trace_cg, span_id_to_index, valid = buildCallGraph(trace)
             if not valid:
-                print("Not valid")
-                # input()
                 overall_not_valid += 1
+            else:
+                overall_valid += 1
+                convertToJaegerFormat(trace, trace_cg, span_id_to_index, dataset_id)
+
+    compress(path + 'traces' + str(dataset_id) + '.tar.lama', path + 'traces' + str(dataset_id) + '/', "w:xz")
+    shutil.rmtree(path + 'traces' + str(dataset_id) + '/')
