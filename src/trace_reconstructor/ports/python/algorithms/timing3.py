a4a6054487c1c7934e2b34c5b5fa5b1a7e5d522d
Sachin Ashok <sachinashok.g@gmail.com>
2023-06-28 18:11:12 -0500

    Exact MIS algorithm based off '95 paper

--- src/trace_reconstructor/ports/python/algorithms/timing3.py
+++ src/trace_reconstructor/ports/python/algorithms/timing3.py
@@ -789,3 +789,95 @@ def FindTopKAssignments(self, in_span, out_eps, out_span_partitions, K):
     def GetWeightedMIS(self, G, weight):
         vcover = approximation.min_weighted_vertex_cover(G, weight=weight)
         return set(G.nodes()).difference(set(vcover))
+
+    def exact_MWIS(self, graph, pi, b_score=0):
+        ''' compute maximum weighted independent set (recursively) using python
+        networkx package. Input items are:
+        - graph, a networkx graph
+        - pi, a dictionary of dual values attached to node (primal constraints)
+        - b_score, a bestscore (if non 0, it pruned some final branches)
+        It returns:
+        - mwis_set, a MWIS as a sorted tuple of nodes
+        - mwis_weight, the sum over n in mwis_set of pi[n]'''
+        global best_score
+        # assert sum(pi) > 0
+        graph_copy = graph.copy()
+        # mwis weight is stored as a 'score' graph attribute
+        graph_copy.graph['score'] = 0
+        best_score = b_score
+
+        def get_mwis(G):
+            '''
+            Based on "A column generation approach for graph coloring" from
+            Mehrotra and Trick, 1995
+            '''
+            global best_score
+            # score stores the best score along the path explored so far
+            key = tuple(sorted(G.nodes()))
+            ub = sum(pi[n] for n in G.nodes())
+            score = G.graph['score']
+            # if graph is composed of singletons, leave now
+            if G.number_of_edges == 0:
+                if score + ub > best_score + EPS:
+                    best_score = score + ub
+                return key, ub
+            # compute highest priority node (used in recursion to choose {i})
+            node_iter = ((n, deg*pi[n]) for (n, deg) in G.degree())
+            node_chosen, _ = max(node_iter, key=lambda x: x[1])
+            pi_chosen = pi[node_chosen]
+            node_chosen_neighbors = list(G[node_chosen])
+            pi_neighbors = sum(pi[n] for n in node_chosen_neighbors)
+            G.remove_node(node_chosen)
+            # Gh = G - {node_chosen} union {anti-neighbors{node-chosen}}
+            # For Gh, ub decreases by value of pi over neighbors of {node_chosen}
+            # and value of pi over {node_chosen} as node_chosen is disconnected
+            # For Gh, score increases by value of pi over {node_chosen}
+            Gh = G.copy()
+            Gh.remove_nodes_from(node_chosen_neighbors)
+            mwis_set_h, mwis_weight_h = tuple(), 0
+            if Gh:
+                ubh = ub - pi_neighbors - pi_chosen
+                if score + pi_chosen + ubh > best_score + EPS:
+                    Gh.graph['score'] += pi_chosen
+                    mwis_set_h, mwis_weight_h = get_mwis(Gh)
+                del Gh
+            mwis_set_h += (node_chosen, )
+            mwis_weight_h += pi_chosen
+            # Gp = G - {node_chosen}
+            # For Gp, ub decreases by value of pi over {node_chosen}
+            # For Gh, score does not increase
+            mwis_set_p, mwis_weight_p = tuple(), 0
+            if G:
+                ubp = ub - pi_chosen
+                if score + ubp > best_score + EPS:
+                    mwis_set_p, mwis_weight_p = get_mwis(G)
+                del G
+            # select case with maximum score
+            if mwis_set_p and mwis_weight_p > mwis_weight_h + EPS:
+                mwis_set, mwis_weight = mwis_set_p, mwis_weight_p
+            else:
+                mwis_set, mwis_weight = mwis_set_h, mwis_weight_h
+            # increase score
+            score += mwis_weight
+            if score > best_score + EPS:
+                best_score = score
+            # return set and weight
+            key = tuple(sorted(mwis_set))
+            return key, mwis_weight
+
+        best_mis = None
+        best_score_1 = -math.inf
+
+        for i in range(1000):
+            try:
+                mis, w = get_mwis(copy.deepcopy(graph_copy))
+            except:
+                assert False
+            score = sum([graph_copy.nodes[n]['weight'] for n in mis])
+            # score = len(mis)
+            if best_mis is None or score > best_score_1:
+                best_mis = mis
+                best_score_1 = score
+
+        return best_mis
+        # return get_mwis(graph_copy)
