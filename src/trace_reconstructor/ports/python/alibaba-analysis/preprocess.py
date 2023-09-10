6364fb0f4ee4fa0d3932246c15fab7e742f39b60
Sachin Ashok <sachinashok.g@gmail.com>
2023-09-10 11:27:12 -0500

    Add some comments

--- src/trace_reconstructor/ports/python/alibaba-analysis/preprocess.py
+++ src/trace_reconstructor/ports/python/alibaba-analysis/preprocess.py
@@ -6,10 +6,12 @@ from pathlib import Path
 def compress(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
+
 def uncompress(output_path, source_file):
     tarfile_object = tarfile.open(source_file)
     tarfile_object.extractall(output_path)
     tarfile_object.close()
+
 def returnOutputPath(trace_id):
     original_shard = original_dataset[trace_id]
     output_path = path + "shard" + str(original_shard) + "/"
@@ -23,8 +25,8 @@ overall_trace_ids = [[] for i in range(num_shards + 1)]
 original_dataset = {}
 
 for dataset_id in range(0, num_shards + 1):
-    uncompress(path, path + "MSCallGraph_" + str(dataset_id) + ".tar.gz")
 
+    uncompress(path, path + "MSCallGraph_" + str(dataset_id) + ".tar.gz")
     Path(path + "shard" + str(dataset_id) + "/").mkdir(parents = True, exist_ok = True)
 
     traces = {}
@@ -33,6 +35,7 @@ for dataset_id in range(0, num_shards + 1):
     with open(path + "MSCallGraph_" + str(dataset_id) + ".csv", mode = "r") as file:
         csv_file = csv.reader(file)
         trace_count = 1
+
         for row in csv_file:
             if row[1] == 'traceid':
                 continue
@@ -47,6 +50,12 @@ for dataset_id in range(0, num_shards + 1):
                 original_dataset[row[1]] = dataset_id
             traces[row[1]].append(row)
 
+    '''
+        The compression, uncompression, and deletion done in this script
+        (and elsewhere) is in a streaming manner to just save space on disk
+        while preprocessing.
+    '''
+
     for i in range(len(trace_ids)):
         lookback_flag = False
         if i % 10000 == 0:
@@ -54,6 +63,10 @@ for dataset_id in range(0, num_shards + 1):
 
         output_path = returnOutputPath(trace_ids[i])
 
+        '''
+            If a compressed file exists for output_path + trace_id, the
+            trace_id has been seen before and straddles multiple shard files
+        '''
         if os.path.isfile(output_path + trace_ids[i] + ".tar.gz"):
             print("Lookback fail: ", trace_ids[i])
             num_lookback_errors += 1
@@ -71,6 +84,10 @@ for dataset_id in range(0, num_shards + 1):
             compress(output_path + str(trace_ids[i]) + ".tar.gz", output_path + str(trace_ids[i]) + ".csv")
             os.remove(output_path + str(trace_ids[i]) + ".csv")
 
+    '''
+        Check if past shards have traces corresponding to the same trace_id
+        within a certain lookback window threshold.
+    '''
     lookback = 5
     if dataset_id >= lookback:
         if dataset_id != num_shards:
